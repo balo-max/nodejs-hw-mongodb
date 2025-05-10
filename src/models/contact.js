a91977790fa2ed66b6e49888346085f7ef01828e
balo-max <shoni6124@gmail.com>
2025-05-10 09:36:24 +0200

    fix

--- src/models/contact.js
+++ src/models/contact.js
@@ -6,7 +6,7 @@ const contactSchema = new mongoose.Schema({
         required: true,
     },
     phoneNumber: {
-        type: Number,
+        type: String,
         required: true,
     },
     email: {
